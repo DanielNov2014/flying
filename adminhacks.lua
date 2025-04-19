a0837fdf605ee6ddad730ab2a11cc2d3e0f81ccc
DanielNov2014 <michaelvinthermoller@gmail.com>
2025-04-19 20:57:58 +0200

    Update adminhacks.lua

--- adminhacks.lua
+++ adminhacks.lua
@@ -18,15 +18,26 @@ local SRC = Instance.new("TextButton")
 local UICorner_6 = Instance.new("UICorner")
 local RCC = Instance.new("TextBox")
 local UICorner_7 = Instance.new("UICorner")
-local SRC_2 = Instance.new("TextButton")
+local Excute = Instance.new("TextButton")
 local UICorner_8 = Instance.new("UICorner")
 local OC = Instance.new("TextButton")
 local UICorner_9 = Instance.new("UICorner")
+local MusicFrame = Instance.new("Frame")
+local UICorner_10 = Instance.new("UICorner")
+local Musicholder = Instance.new("ScrollingFrame")
+local Music = Instance.new("TextButton")
+local UICorner_11 = Instance.new("UICorner")
+local UIListLayout = Instance.new("UIListLayout")
+local Music_2 = Instance.new("TextButton")
+local UICorner_12 = Instance.new("UICorner")
+local Music_3 = Instance.new("TextButton")
+local UICorner_13 = Instance.new("UICorner")
 
 --Properties:
 
 Hackui.Name = "Hackui"
 Hackui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+Hackui.Enabled = true
 Hackui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 Hackui.ResetOnSpawn = false
 
@@ -130,34 +141,33 @@ RCC.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 RCC.BackgroundTransparency = 1.000
 RCC.BorderColor3 = Color3.fromRGB(0, 0, 0)
 RCC.BorderSizePixel = 0
-RCC.ClearTextOnFocus = false
 RCC.Position = UDim2.new(0, 0, 0.550084889, 0)
 RCC.Size = UDim2.new(1, 0, 0.0848896429, 0)
 RCC.ClearTextOnFocus = false
 RCC.Font = Enum.Font.SourceSans
-RCC.PlaceholderText = "run hd admin commands"
+RCC.PlaceholderText = "run client commands"
 RCC.Text = ""
 RCC.TextColor3 = Color3.fromRGB(0, 0, 0)
 RCC.TextSize = 14.000
 
 UICorner_7.Parent = RCC
 
-SRC_2.Name = "ECC"
-SRC_2.Parent = MainFrame
-SRC_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-SRC_2.BackgroundTransparency = 1.000
-SRC_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
-SRC_2.BorderSizePixel = 0
-SRC_2.Position = UDim2.new(0, 0, 0.652386248, 0)
-SRC_2.Size = UDim2.new(1, 0, 0.0848896429, 0)
-SRC_2.Font = Enum.Font.SourceSans
-SRC_2.Text = "Excute"
-SRC_2.TextColor3 = Color3.fromRGB(0, 0, 0)
-SRC_2.TextScaled = true
-SRC_2.TextSize = 14.000
-SRC_2.TextWrapped = true
+Excute.Name = "Excute"
+Excute.Parent = MainFrame
+Excute.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Excute.BackgroundTransparency = 1.000
+Excute.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Excute.BorderSizePixel = 0
+Excute.Position = UDim2.new(0, 0, 0.652386248, 0)
+Excute.Size = UDim2.new(1, 0, 0.0848896429, 0)
+Excute.Font = Enum.Font.SourceSans
+Excute.Text = "Excute"
+Excute.TextColor3 = Color3.fromRGB(0, 0, 0)
+Excute.TextScaled = true
+Excute.TextSize = 14.000
+Excute.TextWrapped = true
 
-UICorner_8.Parent = SRC_2
+UICorner_8.Parent = Excute
 
 OC.Name = "O/C"
 OC.Parent = Hackui
@@ -174,6 +184,90 @@ OC.TextSize = 14.000
 OC.TextWrapped = true
 
 UICorner_9.Parent = OC
+
+MusicFrame.Name = "MusicFrame"
+MusicFrame.Parent = Hackui
+MusicFrame.Visible = false
+MusicFrame.BackgroundColor3 = Color3.fromRGB(43, 43, 43)
+MusicFrame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+MusicFrame.BorderSizePixel = 0
+MusicFrame.Position = UDim2.new(0.424770653, 0, 0.191850588, 0)
+MusicFrame.Size = UDim2.new(0.283486247, 0, 0.628183365, 0)
+
+UICorner_10.Parent = MusicFrame
+
+Musicholder.Name = "Musicholder"
+Musicholder.Parent = MusicFrame
+Musicholder.Active = true
+Musicholder.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Musicholder.BackgroundTransparency = 1.000
+Musicholder.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Musicholder.BorderSizePixel = 0
+Musicholder.Size = UDim2.new(1, 0, 1, 0)
+
+Music.Name = "Music"
+Music.Parent = Musicholder
+Music.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Music.BackgroundTransparency = 1.000
+Music.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Music.BorderSizePixel = 0
+Music.Position = UDim2.new(0, 0, 0.312827677, 0)
+Music.Size = UDim2.new(1, 0, 0.0848896429, 0)
+Music.Font = Enum.Font.SourceSans
+Music.Text = "Tacos"
+Music.TextColor3 = Color3.fromRGB(0, 0, 0)
+Music.TextScaled = true
+Music.TextSize = 14.000
+Music.TextWrapped = true
+
+UICorner_11.Parent = Music
+
+UIListLayout.Parent = Musicholder
+UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+
+Music_2.Name = "Music"
+Music_2.Parent = Musicholder
+Music_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Music_2.BackgroundTransparency = 1.000
+Music_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Music_2.BorderSizePixel = 0
+Music_2.Position = UDim2.new(0, 0, 0.312827677, 0)
+Music_2.Size = UDim2.new(1, 0, 0.0848896429, 0)
+Music_2.Font = Enum.Font.SourceSans
+Music_2.Text = "Boss me (b)"
+Music_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+Music_2.TextScaled = true
+Music_2.TextSize = 14.000
+Music_2.TextWrapped = true
+
+UICorner_12.Parent = Music_2
+
+Music_3.Name = "Music"
+Music_3.Parent = Musicholder
+Music_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Music_3.BackgroundTransparency = 1.000
+Music_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Music_3.BorderSizePixel = 0
+Music_3.Position = UDim2.new(0, 0, 0.312827677, 0)
+Music_3.Size = UDim2.new(1, 0, 0.0848896429, 0)
+Music_3.Font = Enum.Font.SourceSans
+Music_3.Text = "lets go gambling"
+Music_3.TextColor3 = Color3.fromRGB(0, 0, 0)
+Music_3.TextScaled = true
+Music_3.TextSize = 14.000
+Music_3.TextWrapped = true
+
+UICorner_13.Parent = Music_3
+
+for i,v in Musicholder:GetChildren() do
+	if v:IsA("TextButton") then
+		v.MouseButton1Click:Connect(function()
+			print("click")
+			for i,v in game:GetDescendants() do if v:IsA("RemoteFunction") and v.Name == "RequestCommand" then v:InvokeServer(";music "..v:GetAttribute("id")) end end
+			MusicFrame.Visible = false
+		end)
+	end
+end
 
 for i,v in Hackui:GetDescendants() do
 	if v:IsA("TextButton") or v:IsA("TextBox") then
@@ -257,11 +351,14 @@ SRC.MouseButton1Click:Connect(function()
 	end
 end)
 
-SRC_2.MouseButton1Click:Connect(function()
+Excute.MouseButton1Click:Connect(function()
 	for i,v in game:GetDescendants() do if v:IsA("RemoteFunction") and v.Name == "RequestCommand" then v:InvokeServer(RCC.text) end end
 end)
 
 PM.MouseButton1Click:Connect(function()
-	for i,v in game:GetDescendants() do if v:IsA("RemoteFunction") and v.Name == "RequestCommand" then v:InvokeServer(";music 142376088") end end
+	MusicFrame.Visible = true
 end)
+
+
+
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/DanielNov2014/flying/main/adminhacks.lua"))()
